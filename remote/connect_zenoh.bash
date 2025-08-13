f584e78fb1d632079de54da9d8cb9dc76a5a86ad
taikitanaka3 <taiki.tanaka@tier4.jp>
2025-08-14 08:57:47 +0900

    Merge commit 'fd44bd0e5cb1ae79d24fc804b8aa69d2a80becaf'

--- remote/connect_zenoh.bash
+++ remote/connect_zenoh.bash
@@ -3,4 +3,4 @@
 set -e
 SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
 IP_ADDR=$(python3 "${SCRIPT_DIR}/scan_ip_addr.py" "$1")
-zenoh-bridge-ros2dds -e "tcp/${IP_ADDR}:7447"
+zenoh-bridge-ros2dds -e "tcp/${IP_ADDR}:7447" -c zenoh-user.json5
