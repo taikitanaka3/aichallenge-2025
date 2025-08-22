9ce22b9d6c4560495ef4e1d0e2b19b8e38ec9f42
Knorrrr <99851410+knorrrr@users.noreply.github.com>
2025-08-22 17:07:41 +0900

    change script to 2025 (#68)

--- remote/connect_ssh.bash
+++ remote/connect_ssh.bash
@@ -1,12 +1,5 @@
 #!/bin/bash
-# Usage: ./connect_zenoh.bash <target-vehicle (A1 to A8)> <user>
-if [ $# -lt 2 ]; then
-    echo "エラー: 引数が不足しています。" >&2
-    echo "使用法: $0 <target-vehicle (A1 to A8)> <user>" >&2
-    exit 1
-fi
-set -e
-SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
-IP_ADDR=$(python3 "${SCRIPT_DIR}/detail/scan_ip_addr.py" "$1")
-echo "Executing command: ssh -AX $2@${IP_ADDR}"
-ssh -AX "$2@${IP_ADDR}"
+ssh -ACY -p 10022 \
+    -o ServerAliveInterval=60 \
+    -o ServerAliveCountMax=3 \
+    tier4@57.180.63.135
